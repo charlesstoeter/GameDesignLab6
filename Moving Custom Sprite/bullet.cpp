9f5d217c5c244686c11f3cca6a90fa413710d31d
Charles Stoeter <charlesstoeter@gmail.com>
2025-05-28 21:26:00 -0400

    finished the bitmap bullet function and applying the function replace bullet

--- Moving Custom Sprite/bullet.cpp
+++ Moving Custom Sprite/bullet.cpp
@@ -28,7 +28,7 @@ void bullet::erase_bullet()
 int bullet::move_bullet (int arrowX, int arrowY, int width, int length, int height)
 {
 	y++;
-	al_draw_filled_rectangle(x,y,x+5,y+5,al_map_rgb(255,255,0));//yellow color
+	al_draw_bitmap(bullet_bmp, x, y, 0);
 
 
 	if (x > arrowX && x < arrowX+width && y > arrowY && y < arrowY+length) {
@@ -49,6 +49,22 @@ int bullet::move_bullet (int arrowX, int arrowY, int width, int length, int heig
 		std::cerr << "Failed to create bullet bitmap.\n";
 		return;
 	}
+
+	// Set the target bitmap to the bullet bitmap
+	al_set_target_bitmap(bullet_bmp);
+
+	//blue background
+	al_clear_to_color(al_map_rgb(0, 0, 255));
+
+	// yellow filled circle (center)
+	al_draw_filled_circle(8, 8, 5, al_map_rgb(255, 255, 0));
+
+	//red diagonal line
+	al_draw_line(2, 2, 14, 14, al_map_rgb(255, 0, 0), 2);
+
+	// Restore drawing to backbuffer (main screen)
+	al_set_target_backbuffer(al_get_current_display());
+
 	
 
 }
